c840c87338a0884f4bd00d761b5f586a9f7a48f9
Stas <stassss2009@gmail.com>
2019-09-22 01:26:04 +0300

    Lab2 init

---
+++ Lab2/cmake-build-debug/CMakeFiles/Lab2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2.dir/main.cpp.obj"
+  "Lab2.pdb"
+  "Lab2.exe"
+  "Lab2.exe.manifest"
+  "libLab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

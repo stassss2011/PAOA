8ff07c4616e077846154424cb85579c145c85cde
Stas <stassss2009@gmail.com>
2019-11-16 01:15:56 +0200

    Lab4

--- Lab4/main.py
+++ Lab4/main.py
@@ -5,34 +5,62 @@ import sys
 
 
 #
+#
+# """
+# 1
+# abracadabra
+# 1
+# 1
+# """
 # Complete the findStrings function below.
 #
-def findStrings(w, queries):
-    S1 = []
-    for w1 in w:
-        # print(w1)
-        # print(len(w1))
-        for i in range(len(w1)):
-            t = 0
-            y = i + 1
-            # print(i)
-            while y <= len(w1):
-                # print(w1[t:y])
-                if (w1[t:y] in S1) == 0:
-                    S1.append(w1[t:y])
-                t += 1
-                y += 1
-    S1.sort()
-    # print(S1)
-    ret=[]
-    for q in queries:
-        if q > (len(S1) + 1):
-            #print("--INVALID--")
-            ret.append("INVALID")
+def suffix_array(w):
+    suffix = []
+    for i in range(len(w)):
+        suf_id = sorted(range(len(w[i])), key=lambda x: w[i][x:])
+        for j in suf_id:
+            suffix.append(w[i][suf_id[j]:len(w[i])])
+    return sorted(list(set(suffix)))
+
+
+def lcp_array(suffix):
+    lcp = [0] * len(suffix)
+    for i in range(len(suffix) - 1):
+        j = 0
+        # while suffix[i][j] == suffix[i + 1][j]:
+        #     j += 1
+        #     lcp[i] += 1
+        if len(suffix[i]) <= len((suffix[i + 1])):
+            for k in range(len(suffix[i]) - 1, 0):
+                if suffix[i + 1].startwitn(suffix[i][0:k]):
+                    lcp[i] = k + 1
         else:
-            #print(S1[2])
-            ret.append(S1[q - 1])
+            for k in range(len(suffix[i + 1]) - 1, 0):
+                if suffix[i].startwitn(suffix[i + 1][0:k]):
+                    lcp[i] = k + 1
+    return lcp
+
+
+def findStrings(w, queries):
+    w = list(map(lambda x: x + '$', w))
+    suffix = suffix_array(w)
+    lcp = lcp_array(suffix)
+    subcnt = 1 - len(suffix)
+    for i in range(1, len(suffix)):
+        subcnt += len(suffix[i]) - lcp[i - 1]
+    ret = []
+    for i in range(len(queries)):
+        if queries[i] > subcnt:
+            ret.append("invalid")
+            continue
+        index = 0
+        j = 1
+        while index + len(suffix[j]) - 1 - lcp[j-1] < queries[i]:
+            index += len(suffix[j]) - 1 - lcp[j-1]
+            j += 1
+        ret.append(suffix[j][0:queries[i] - index + lcp[j - 1]])
     return ret
+
 
 w_count = int(input())
 
@@ -54,4 +82,3 @@ result = findStrings(w, queries)
 
 print('\n'.join(result))
 print('\n')
-
